4e16d624882b0b2d4f0aeffcaec1c4fd2b406fc0
Felix Bello <felix.bello@becit.ch>
2015-11-20 23:15:01 +0100

    added some rooms

--- src/Hunter.java
+++ src/Hunter.java
@@ -11,6 +11,22 @@ public class Hunter {
 		Raum draussen = new Raum("draussen");
 		draussen.setAusgang("north", true);
 		System.out.println(draussen.getAusgang());
+		
+		// Raum Wohnzimmer Createn
+		Raum wohnZimmer = new Raum("wohnzimmer");
+		wohnZimmer.setAusgang("south", true);
+		
+		// Raum Badezimmer Createn
+		Raum badeZimmer = new Raum("badezimmer");
+		badeZimmer.setAusgang("south", true);
+		
+		// Raum Schlafzimmer Createn
+		Raum schlafZimmer = new Raum("schlafzimmer");
+		schlafZimmer.setAusgang("sout", true);
+		
+		// Raum Kueche Createn
+		Raum kueche = new Raum("kueche");
+		kueche.setAusgang("south", true);
 
 		// create a new player
 		Spieler newPlayer = new Spieler();
