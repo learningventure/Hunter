ae8b3efeb18d5fe481577cbd03191f5700a025c0
gilles.brunner <gilles.brunner@students.ffhs.ch>
2015-11-20 22:17:37 +0100

    gamer second attempt

--- src/Hunter.java
+++ src/Hunter.java
@@ -4,17 +4,18 @@
  */
 public class Hunter {
 
-	public Spieler newGamer;
+
 
 	public Hunter() {
 		// TODO Auto-generated constructor stub
-		new Spieler("Felix");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Spieler newGamer;
+		newGamer = new Spieler("Felix");
 		System.out.println("Der Name des ersten und einzigen Spielers ist: " + newGamer);
 	}
 
