3453ff7ec8ca9270d0f653d946f4f25b837a231f
Felix Bello <felix.bello@becit.ch>
2015-11-20 11:59:41 +0100

    started with projec

---
+++ src/Hunter.java
@@ -0,0 +1,22 @@
+/**
+ * @author Felix Bello, Gilles Brunner
+ * @version 0.1
+ */
+public class Hunter {
+
+	/**
+	 * 
+	 */
+	public Hunter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
