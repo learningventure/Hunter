c36dec58c3b3d7de9ca7266c1adac1b64a9eff29
Felix Bello <felix.bello@becit.ch>
2015-11-20 23:32:33 +0100

    some updates

--- src/Spieler.java
+++ src/Spieler.java
@@ -1,10 +1,20 @@
 public class Spieler{
     private String name;
-    private Raum startPunkt;
+    private String startPunkt;
 
     public Spieler(){
-        name = "NoName";
-        startPunkt = null;
+    	this.name = "NoName";
+    	this.startPunkt = null;
+    }
+    
+    public Spieler(String name){
+        this.name = name;
+        this.startPunkt = "draussen";
+    }
+    
+    public Spieler(String name, String startPunkt){
+    	this.name = name;
+    	this.startPunkt = startPunkt;
     }
 
     public void setName(String name){
