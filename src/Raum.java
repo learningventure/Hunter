5bbdeca172e00b9c5061d38971333a90c51e6575
Felix Bello <felix.bello@becit.ch>
2015-11-20 21:46:30 +0100

    class created and variable definition

---
+++ src/Raum.java
@@ -0,0 +1,18 @@
+/*
+ * @author Felix Bello
+ * @version 0.1
+ */
+public class Raum {
+	
+	// Variable definition
+	private String name;
+	private boolean ausNord;
+	private boolean ausSued;
+	private boolean ausWest;
+	private boolean ausOst;
+	
+	public Raum() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
